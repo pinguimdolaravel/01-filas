7c3c95410572fd51137f523d812fd7e44ccf2b9d
Rafael Lunardelli <rafael@devsquad.com>
2021-09-30 16:34:25 -0600

    layout

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,10 @@
 <?php
 
+use Illuminate\Bus\BatchRepository;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (BatchRepository $batchRepository) {
+    return view('welcome', [
+        'batches' => $batchRepository->get(),
+    ]);
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
